882d8d4e17367e790aab0f112a958127ecd560ed
eball <liuy102@hotmail.com>
2025-05-07 21:07:55 +0800

    feat: support mdns proxy

--- overlay/usr/bin/start-sunshine.sh
+++ overlay/usr/bin/start-sunshine.sh
@@ -44,6 +44,7 @@ fi
 
 if [ ! -f "${USER_HOME:?}/.config/sunshine/sunshine.conf" ]; then
     cp -vf /templates/sunshine/sunshine.conf "${USER_HOME:?}/.config/sunshine/sunshine.conf"
+    echo "external_ip = ${NODE_IP}" >> "${USER_HOME:?}/.config/sunshine/sunshine.conf"
 fi
 if [ ! -f "${USER_HOME:?}/.config/sunshine/apps.json" ]; then
     cp -vf /templates/sunshine/apps.json "${USER_HOME:?}/.config/sunshine/apps.json"
