3ad2e5074a33c41d74ca3288c5b55c82130f8602
eball <liuy102@hotmail.com>
2026-01-04 21:57:12 +0800

    feat: add wait_for_pulse function to improve pulseaudio startup reliability for Olares One

--- overlay/usr/bin/start-pulseaudio.sh
+++ overlay/usr/bin/start-pulseaudio.sh
@@ -23,9 +23,26 @@ echo "PULSEAUDIO: Starting pulseaudio service"
 /usr/bin/pulseaudio --exit-idle-time=-1 &
 pulseaudio_pid=$!
 
+
+wait_for_pulse() {
+    MAX=60 # About 30 seconds
+    CT=0
+    while ! pactl stat >/dev/null 2>&1; do
+        sleep 0.50s
+        CT=$(( CT + 1 ))
+        if [ "$CT" -ge "$MAX" ]; then
+            echo "FATAL: $0: Gave up waiting for pulse audio"
+            kill -TERM "$pulseaudio_pid" 2>/dev/null
+            exit 11
+        fi
+    done
+}
+
 if [[ "${DEVICE_NAME}" = "Olares One" ]]; then
+    echo "PULSEAUDIO: Setting Olares One HDMI audio output"
+    wait_for_pulse
     # Set HDMI audio output
-    pactl load-module module-alsa-sink device=plughw:0,3 sink_name=nvhdmi
+    pactl load-module module-alsa-sink device=plughw:0,3 sink_name=nvhdmi || { kill -TERM "$pulseaudio_pid" 2>/dev/null && exit 12; }
     amixer -c 0 sset 'IEC958' on
     pactl unload-module module-alsa-sink
 fi
